c122df53e1f2e15a39244ab3a096cadc4caee7da
raenonx0710@gmail.com <raenonx0710@gmail.com>
2020-04-15 00:31:58 -0500

    ADD - SearchSyntax classes ADD - JUnit lib jars

---
+++ searchsyntax/src/FilterElement.java
@@ -0,0 +1,11 @@
+/**
+ * The smallest unit of the search syntax.
+ */
+public interface FilterElement {
+    /**
+     * Get the syntax of an element.
+     *
+     * @return syntax of an element
+     */
+    String getSyntax();
+}
---
+++ searchsyntax/tests/FilterParameterTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilterParameterTest {
+    @Test
+    void test_parse() {
+        // FIXME: Subtest?
+    }
+}
