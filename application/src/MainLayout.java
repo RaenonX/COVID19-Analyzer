57817771be279c4205c0024fb2cf4b8d5da427b0
raenonx0710@gmail.com <raenonx0710@gmail.com>
2020-04-26 21:06:50 -0500

    ADD - #23 Dialog prototype IMP - Updated data source file
    Signed-off-by: raenonx0710@gmail.com <raenonx0710@gmail.com>

--- application/src/MainLayout.java
+++ application/src/MainLayout.java
@@ -94,7 +94,6 @@ public class MainLayout extends LayoutBase {
             setOnAction(e -> FilterSyntaxDocGUI.documentationPopup(stage).show());
         }};
         Button b2_export = new Button("Export Result") {{
-            // FIXME: Display a summary text preview dialog
             setId("export");
             setOnAction(e -> onExportClicked());
         }};
