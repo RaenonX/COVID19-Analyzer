72b2a8a2b5caa8441500108172d13c5c196ac557
raenonx0710@gmail.com <raenonx0710@gmail.com>
2020-04-27 23:43:44 -0500

    ADD - Build Artifacts
    Signed-off-by: raenonx0710@gmail.com <raenonx0710@gmail.com>

--- run-jar.bat
+++ run-jar.bat
@@ -1,5 +1,5 @@
+@ECHO OFF
 SET /P jfx11path="JavaFX 11 Lib Path > "
-ECHO %jfx11path%
 java^
     --module-path "%jfx11path%"^
     --add-modules javafx.controls,javafx.fxml,javafx.graphics,javafx.web^
